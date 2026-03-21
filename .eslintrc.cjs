41be89ab53f129fe54acfd647df88d3f28660ffe
Jhonatan Santos <wazinsky@outlook.com>
2026-03-20 22:48:23 -0300

    skeleton files

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  env: { browser: true, node: true, es2022: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  settings: { react: { version: 'detect' } },
+  rules: {},
+};
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # patent-workbench
+
 A local-first visual shell for LLMs focused on patent ideation and drafting workflows.
